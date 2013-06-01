eeeb03d3b21edc7d3fe67eaae092ada8595e321a
grandbora <bora.tunca@yahoo.com>
2013-06-01 22:18:51 +0200

    migrate !

--- db/migrate/20130601192923_alter_column.rb
+++ db/migrate/20130601192923_alter_column.rb
@@ -1,23 +1,35 @@
 class AlterColumn < ActiveRecord::Migration
-  def up
-    change_table :items do |t|
-      change_column :image, :text, :limit => nil
-      change_column :source_url, :text, :limit => nil
-      change_column :title, :text, :limit => nil
-      change_column :subtitle, :text, :limit => nil
-      change_column :image, :text, :limit => nil
-      change_column :audio, :text, :limit => nil
-    end
-  end
 
-  def down
+  def change
     change_table :items do |t|
-      change_column :image, :string
-      change_column :source_url, :string
-      change_column :title, :string
-      change_column :subtitle, :string
-      change_column :image, :string
-      change_column :audio, :string
+      t.change :image, :text, 
+      t.change :source_url, :text
+      t.change :title, :text
+      t.change :subtitle, :text
+      t.change :image, :text
+      t.change :audio, :text
     end
   end
+  
+  # def up
+  #   change_table :items do |t|
+  #     change_column :image, :text, 
+  #     change_column :source_url, :text
+  #     change_column :title, :text
+  #     change_column :subtitle, :text
+  #     change_column :image, :text
+  #     change_column :audio, :text
+  #   end
+  # end
+
+  # def down
+  #   change_table :items do |t|
+  #     change_column :image, :string
+  #     change_column :source_url, :string
+  #     change_column :title, :string
+  #     change_column :subtitle, :string
+  #     change_column :image, :string
+  #     change_column :audio, :string
+  #   end
+  # end
 end
