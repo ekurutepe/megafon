9290c0cd9410e90743743cffac4f698c6e86f79f
grandbora <bora.tunca@yahoo.com>
2013-06-01 22:22:15 +0200

    migrate !

--- db/migrate/20130601192923_alter_column.rb
+++ db/migrate/20130601192923_alter_column.rb
@@ -2,34 +2,12 @@ class AlterColumn < ActiveRecord::Migration
 
   def change
     change_table :items do |t|
-      t.change :image, :text, 
-      t.change :source_url, :text
-      t.change :title, :text
-      t.change :subtitle, :text
-      t.change :image, :text
-      t.change :audio, :text
+      t.change :image, :string, :limit => 5000
+      t.change :source_url, :string, :limit => 5000
+      t.change :title, :string, :limit => 5000
+      t.change :subtitle, :string, :limit => 5000
+      t.change :image, :string, :limit => 5000
+      t.change :audio, :string, :limit => 5000
     end
   end
-  
-  # def up
-  #   change_table :items do |t|
-  #     change_column :image, :text, 
-  #     change_column :source_url, :text
-  #     change_column :title, :text
-  #     change_column :subtitle, :text
-  #     change_column :image, :text
-  #     change_column :audio, :text
-  #   end
-  # end
-
-  # def down
-  #   change_table :items do |t|
-  #     change_column :image, :string
-  #     change_column :source_url, :string
-  #     change_column :title, :string
-  #     change_column :subtitle, :string
-  #     change_column :image, :string
-  #     change_column :audio, :string
-  #   end
-  # end
 end
