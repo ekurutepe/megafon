3a5463cf4aea69b15215c273d6fd368b1676b52a
Engin Kurutepe <engin@kurutepe.com>
2013-06-01 23:11:40 +0200

    update column types

--- db/migrate/20130601151312_create_items.rb
+++ db/migrate/20130601151312_create_items.rb
@@ -2,10 +2,10 @@ class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
       t.string :source_type
-      t.string :media
-      t.string :source_url
-      t.string :title
-      t.string :subtitle
+      t.text :media
+      t.text :source_url
+      t.text :title
+      t.text :subtitle
       t.timestamp :timestamp
 
       t.timestamps
