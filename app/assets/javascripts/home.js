5c8fe2d1d65ef69464cae791b8fbb4ca006cf547
grandbora <bora.tunca@yahoo.com>
2013-06-01 19:11:04 +0200

    path fixes

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,19 +3,46 @@
 
 
 $(document).ready(function () {
-  $('#myGallery').rfg({
-    imageWidth:350,
-    center:true,
-    spacing:5,
-    categories:false,
-    categoryOptions:{
-      defaultCategory:'All',
-      includeAll: true
-    },
-    flickrOptions: {
-      imageSize: 'l'
-    },
-    lightbox:true,
-    initialHeight:1200
-  });
+  // $('#myGallery').rfg({
+  //   imageWidth:350,
+  //   center:true,
+  //   spacing:5,
+  //   categories:false,
+  //   categoryOptions:{
+  //     defaultCategory:'All',
+  //     includeAll: true
+  //   },
+  //   flickrOptions: {
+  //     imageSize: 'l'
+  //   },
+  //   lightbox:true,
+  //   initialHeight:1200
+  // });
+      $('#myGallery').rfg({
+        imageWidth:350,
+        center:true,
+        spacing:5,
+        categories:false,
+        categoryOptions:{
+          defaultCategory:'All',
+          includeAll: true
+        },
+        flickrSets: [{
+              setId: '72157633836081300',
+              apiKey: 'bb8ba80ac9f7b4da1e58e45e55ae6d6a'
+            },
+            {
+              setId: '72157633834703987',
+              apiKey: 'bb8ba80ac9f7b4da1e58e45e55ae6d6a'
+            }],
+        flickrOptions: {
+          useTitle: true,
+          imageSize: 'l'
+        },
+        lightbox:true,
+        initialHeight:1200
+      });
+
+
+
 });
