166ff110ea07d469084cbc71dcea0e358a86fe27
Engin Kurutepe <engin@kurutepe.com>
2013-06-01 15:27:40 +0200

    fix strings in twitter init

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,6 @@
 Twitter.configure do |config|
-  config.consumer_key = Rb3URpZhjaX6BU3AvE9A
-  config.consumer_secret = F8uavclWeSzXzpz5KynPcGXwFLR2A6CqyjibWLz7HA
+  config.consumer_key = 'Rb3URpZhjaX6BU3AvE9A'
+  config.consumer_secret = 'F8uavclWeSzXzpz5KynPcGXwFLR2A6CqyjibWLz7HA'
   # config.oauth_token = YOUR_OAUTH_TOKEN
   # config.oauth_token_secret = YOUR_OAUTH_TOKEN_SECRET
 end
