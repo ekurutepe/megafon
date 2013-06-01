dec2d7945889758e81a8d0eb3ca3571dbeb6ddd0
Engin Kurutepe <engin@kurutepe.com>
2013-06-01 22:56:32 +0200

    cancel out migration

--- db/migrate/20130601192923_alter_column.rb
+++ db/migrate/20130601192923_alter_column.rb
@@ -1,24 +1,24 @@
 class AlterColumn < ActiveRecord::Migration
 
   def up
-    change_table :items do |t|
-      t.change :image, :text, :limit => nil
-      t.change :source_url, :text, :limit => nil
-      t.change :title, :text, :limit => nil
-      t.change :subtitle, :text, :limit => nil
-      t.change :image, :text, :limit => nil
-      t.change :audio, :text, :limit => nil
-    end
+    # change_table :items do |t|
+    #   t.change :image, :text, :limit => nil
+    #   t.change :source_url, :text, :limit => nil
+    #   t.change :title, :text, :limit => nil
+    #   t.change :subtitle, :text, :limit => nil
+    #   t.change :image, :text, :limit => nil
+    #   t.change :audio, :text, :limit => nil
+    # end
   end
 
   def down
-    change_table :items do |t|
-      t.change :image, :string
-      t.change :source_url, :string
-      t.change :title, :string
-      t.change :subtitle, :string
-      t.change :image, :string
-      t.change :audio, :string
-    end
+    # change_table :items do |t|
+    #   t.change :image, :string
+    #   t.change :source_url, :string
+    #   t.change :title, :string
+    #   t.change :subtitle, :string
+    #   t.change :image, :string
+    #   t.change :audio, :string
+    # end
   end
 end
