9f20bdc461240bb8cd17704e076e9da97b05ad51
Engin Kurutepe <engin@kurutepe.com>
2013-06-01 16:13:40 +0200

    can now get a json of tweets with photos for a given hashtag

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,21 @@
 class ItemsController < ApplicationController
   def index
-    render :json => {:text => 'hello world'}
+    tweets = Twitter.search(params[:hashtag], :count => 20, :result_type => 'popular', :filter => 'links', :include_entities => true)
+    
+    
+
+    filtered_tweets = []
+    tweets[:statuses].each do |t|
+      media_items = t[:media]
+      media_items.each do |m|
+        if m.instance_of?(Twitter::Media::Photo) 
+          filtered_tweets << t
+        end
+      end
+    end
+    render :json => filtered_tweets
   end
+  
+  
 end
+
