79e9cb1e490031800f92150e73e1e3c3b0dea325
grandbora <bora.tunca@yahoo.com>
2013-06-01 14:22:18 +0200

    json syntax

--- app/controllers/mockup_controller.rb
+++ app/controllers/mockup_controller.rb
@@ -1,15 +1,15 @@
 class MockupController < ApplicationController
   def index
-    render :json => [
+    render :json => '[
       {
-       "source_type": 'twitter',
-       "media": 'http://api.soundcloud.com/tracks/13158665.json',
-       "source_url": 'where to link, open page',
-       "title": 'mockup title',
-       "subtitle": 'mockup SUBBBtitle',
-       "timestamp": ""
+       "source_type": "twitter",
+       "media": "http://api.soundcloud.com/tracks/13158665.json",
+       "source_url": "where to link, open page",
+       "title": "mockup title",
+       "subtitle": "mockup SUBBBtitle",
+       "timestamp": "asd"
       }
-    ]
+    ]'
   end
 
 end
