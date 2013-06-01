5e77ba47d6ae6f2ea04f3c0b5e1ada17704f8ffb
grandbora <bora.tunca@yahoo.com>
2013-06-01 22:26:04 +0200

    migrate !

--- db/migrate/20130601192923_alter_column.rb
+++ db/migrate/20130601192923_alter_column.rb
@@ -1,13 +1,24 @@
 class AlterColumn < ActiveRecord::Migration
 
-  def change
+  def up
     change_table :items do |t|
-      t.change :image, :string, :limit => 5000
-      t.change :source_url, :string, :limit => 5000
-      t.change :title, :string, :limit => 5000
-      t.change :subtitle, :string, :limit => 5000
-      t.change :image, :string, :limit => 5000
-      t.change :audio, :string, :limit => 5000
+      change_column :image, :string, :limit => 5000
+      change_column :source_url, :string, :limit => 5000
+      change_column :title, :string, :limit => 5000
+      change_column :subtitle, :string, :limit => 5000
+      change_column :image, :string, :limit => 5000
+      change_column :audio, :string, :limit => 5000
+    end
+  end
+
+  def down
+    change_table :items do |t|
+      change_column :image, :string
+      change_column :source_url, :string
+      change_column :title, :string
+      change_column :subtitle, :string
+      change_column :image, :string
+      change_column :audio, :string
     end
   end
 end
